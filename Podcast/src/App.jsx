2ff07bffd13940bb2363575eb5ee3b04560a2150
Thabang Selowa <mohale20cobra@gmail.com>
2024-10-17 21:27:05 +0200

    Renamed showlist file

--- Podcast/src/App.jsx
+++ Podcast/src/App.jsx
@@ -1,16 +1,14 @@
-import React from "react";
-import PodcastList from "./components/PodcastList";
-import Footer from "./components/Footer";
-//import Header from "./components/Header";
-
-export default function App() {
-
-return (
-  <div>
-    
-    <PodcastList />
-    <Footer />
-  </div>
-)
-
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { Theme } from '@radix-ui/themes';
+import Home from './components/Home';
+import ShowList from './components/ShowList';
+import ShowDetails from './components/ShowDetails';
+import Favorites from './components/Favorites';
+import ThemeToggle from './components/ThemeToggle';
+import AudioPlayer from './components/AudioPlayer';
+import CompletedEpisodes from './components/CompletedEpisodes';
+import SearchBar from './components/SearchBar';
+import '@radix-ui/themes/styles.css';
+import './App.css';
 }
